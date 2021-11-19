5675ac35b85c4e0df26a524ebf54a28db44166c0
rudrOwO <rudro1152@gmail.com>
2021-11-19 18:10:29 +0600

    Improved ? OOP structure of Board

--- UI.js
+++ UI.js
@@ -2,9 +2,7 @@ let mainBoard = new Board();
 const canvasWidth = 240;
 const canvasHeight = 240;
 const squareSide = canvasWidth / 3;
-const compChar = 'x';
 const computer = Symbol('computer');
-const playerChar = 'o';
 const player = Symbol('player');
 
 
@@ -21,7 +19,7 @@ function touchStarted() {
     
     // Check Click/Touch bounds
     if (squareRow >= 0 && squareRow < 3 && squareCol >= 0 && squareCol < 3)
-        mainBoard.data[squareRow][squareCol] = playerChar;
+        mainBoard.set(squareRow, squareCol, player);
 }
 
 
@@ -30,7 +28,7 @@ function draw() {
     for (let y = 0; y < canvasHeight; y += squareSide) {
         for (let x = 0; x < canvasWidth; x += squareSide) {
             square(x, y, squareSide);
-            let displayText = mainBoard.data[y / squareSide][x / squareSide];
+            let displayText = mainBoard.get(y / squareSide, x / squareSide);
             if (displayText !== null)
                 text(displayText, x + squareSide / 2, y + squareSide / 2);
         }
--- board.js
+++ board.js
@@ -1,15 +1,32 @@
 class Board {
+    #grid;
+    saturation;
+
     constructor(other = null) {
         // Copy Constructor
         if (other !== null) {
-            Object.assign(this, other);
+            this.saturation = other.saturation;
+            Object.assign(this.#grid, other.get());
             return;
         }
 
         // Default Constructor
-        this.data = new Array();
+        this.saturation = 0;
+        this.#grid = new Array();
         for (let i = 0; i < 3; ++i) 
-            this.data.push(new Array(null, null, null));
+            this.#grid.push(new Array(null, null, null));
     }
 
+    // Getter for a specific cell; Call with no parameters to get the entire grid
+    get(i = null, j = null) {
+        if (i === null && j === null)
+            return this.#grid;
+        return this.#grid[i][j];
+    }
+
+    // Setter for a specific cell;
+    set(i, j, turn) {
+        this.#grid[i][j] = (turn == player ? 'o' : 'x');
+        ++this.saturation;
+    }
 }
